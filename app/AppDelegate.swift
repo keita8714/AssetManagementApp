c3f99043b768503d887878c8c04e0ae7384339fc
mk <munekawa0807@outlook.jp>
2020-08-27 23:46:02 +0900

    変更

--- app/AppDelegate.swift
+++ app/AppDelegate.swift
@@ -16,8 +16,8 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
         // Override point for customization after application launch.
-        return true
         GADMobileAds.sharedInstance().start(completionHandler: nil)
+        return true
     }
 
     // MARK: UISceneSession Lifecycle
